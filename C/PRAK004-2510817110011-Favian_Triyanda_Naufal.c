0e6818bdd3759bf4eb92cdf3e39a069e99e5b886
FAVIAN TRIYANDA NAUFAL <viannoval446@gmail.com>
2025-09-26 22:26:56 +0800

    Add files via upload

---
+++ C/PRAK004-2510817110011-Favian_Triyanda_Naufal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("############################## \n");
+    printf("#                            # \n");
+    printf("#   Favian Triyanda Naufal   # \n");
+    printf("#       2510817110011        # \n");
+    printf("#                            # \n");
+    printf("############################## \n");
+    
+    return 0;
+}
